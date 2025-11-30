65228e49f7eb2cb7e4d651f4489a10574081c840
Wasim2934 <mdwasimlesnar0019@gmail.com>
2025-12-01 01:54:28 +0530

    first commit

---
+++ images/README.md
@@ -0,0 +1,27 @@
+# 🔐 Random Password Generator
+
+A clean and functional **Random Password Generator App** built using **HTML, CSS, and JavaScript**.  
+This tool instantly creates secure, unpredictable passwords and lets users copy them with one click.  
+Designed with a modern, responsive UI and smooth user experience.
+
+## Live Demo -  https://wasim2934.github.io/random-password-generator/
+
+## Features
+
+- Generate strong random passwords
+- Includes uppercase, lowercase, digits & symbols
+- One-click **Copy to Clipboard**
+- Modern & minimal UI
+- Fast, lightweight and beginner-friendly code
+- Responsive on all screen sizes
+
+## Tech Stack
+
+- **HTML5**
+- **CSS3** 
+- **JavaScript 
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,38 @@
+let passwordBox = document.querySelector("#password");
+let button = document.querySelector("#btn");
+let passLength = 15;
+
+let upperCase = "ABCDEFGHOJKLMNOPQRSTUVWXYZ";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let number = "012346789";
+let symbol = "!@#$%^&*()_-=+[]{}|:;<>?/";
+
+let allChar = upperCase + lowerCase + number + symbol;
+
+function randomPassword() {
+  let password = "";
+
+  password += upperCase[Math.floor(Math.random()*upperCase.length)]
+  password += lowerCase[Math.floor(Math.random()*lowerCase.length)]
+  password += number[Math.floor(Math.random()*number.length)]
+  password += symbol[Math.floor(Math.random()*symbol.length)]
+
+  while (password.length < passLength) {
+    password += allChar[Math.floor(Math.random()*allChar.length)];
+  }
+  passwordBox.value = password;
+}
+
+button.addEventListener("click", () => {
+  randomPassword();
+})
+
+let copyPass = document.querySelector("#copy");
+
+copyPass.addEventListener("click", () => {
+  passwordBox.select();
+  document.execCommand("copy");
+})
+
+
+ 
